fdccf9b8e86e1fbb6ac881238bd43b3ee67f967a
bhanukandregula <bhanuprakashkandregula@gmail.com>
2022-11-29 12:50:39 -0500

    GET all stores API

--- app.py
+++ app.py
@@ -4,22 +4,43 @@ app = Flask(__name__)
 
 stores = [
     {
-        "name": "My Store",
+        "name": "Ikea",
         "items": [
             {
-                "item_name": "Chair",
-                "item_price": 19.99
+                "item_name": "Vegetables Filter",
+                "item_price": 05.89
+            },
+            {
+                "item_name": "Ice pops",
+                "item_price": 06.99
+            }
+        ]
+    },
+    {
+        "name": "Best Buy",
+        "items": [
+            {
+                "item_name": "Bose Mini Speaker",
+                "item_price": 79.99
+            },
+            {
+                "item_name": "Sony A7C Digital Mirorless Camera",
+                "item_price": 1800.00
             }
         ]
     }
 ]
 
 
-@app.get("/store")
-def get_store():
+# This will fetch all the stores we have in dictionary defined above
+@app.get("/stores")
+def get_stores():
     # order of the key value might be different when it renders on the response
     # it shouldn't be always same
     return {"stores": stores}
+
+# let's create a store and append to dictionary list
+
 
 
 if __name__ == '__main__':
