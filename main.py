78480ccbeeabd09e996d7b77f7c74d47300926d7
bhanukandregula <bhanuprakashkandregula@gmail.com>
2022-12-05 19:04:14 -0500

    One-Many relationship between stores and items

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBluePrint
 
 
 # request
@@ -43,6 +44,7 @@ def create_app(db_url=None):
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBluePrint)
 
     return app
 
--- schemas.py
+++ schemas.py
@@ -4,22 +4,27 @@ from marshmallow import Schema, fields
 
 class PlainItemSchema(Schema):
     # we will use this value while we are returning the data as response
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     item_name = fields.Str(required=True)
     item_price = fields.Float(required=True)
     # store_id = fields.Str(required=True)
+
+
+class PlainStoreSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+
+
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
 
 
 class ItemUpdateSchema(Schema):
     # We don't need to validate both, we only need one
     item_name = fields.Str()
     item_price = fields.Float()
-    store_id = fields.Int() # optional
-
-
-class PlainStoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+    store_id = fields.Int()  # optional
 
 
 class ItemSchema(PlainItemSchema):
@@ -29,3 +34,9 @@ class ItemSchema(PlainItemSchema):
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
